6cd15b4a920f50ccc6c36c3eed525af03aee1280
James Barlow <98394470+rallosdrake@users.noreply.github.com>
2022-11-29 10:10:23 +0000

    adds basic state logic to data slate

--- src/components/GruntDataSlate.js
+++ src/components/GruntDataSlate.js
@@ -1,12 +1,27 @@
 import { grunt } from "../Utils/solider";
-import React from "react";
+import React, { useState } from "react";
 
 export default function GruntDataSlate() {
+  const [gruntDataState, setGruntDataState] = useState({
+    movement: 5,
+    hitpoints: 1,
+  });
+
+  const handleClick = () => {
+    setGruntDataState((prevValues) => ({
+      ...prevValues,
+      hitpoints: prevValues.hitpoints + 1,
+    }));
+  };
   return (
     <div>
       <form>
-        <th>{grunt.name}</th>,<th>{grunt.armour}</th>,<th>{grunt.attacks}</th>
+        <th>{gruntDataState.movement}</th>
+        <th>{gruntDataState.hitpoints}</th>
       </form>
+      <span>
+        <button onClick={handleClick}>Add Hitpoint!</button>
+      </span>
     </div>
   );
 }
